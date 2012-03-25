4ae151c8416ce3385bdcdfbd3f44c336b96554fe
Connor O'Sullivan <trazmick@gmail.com>
2012-03-24 22:47:01 -0700

    Initial Commit

---
+++ StockSearch/StockSearchViewController.h
@@ -0,0 +1,13 @@
+//
+//  StockSearchViewController.h
+//  StockSearch
+//
+//  Created by Connor O'Sullivan on 3/24/12.
+//  Copyright (c) 2012 trazmick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StockSearchViewController : UIViewController
+
+@end
---
+++ StockSearch/StockSearchViewController.m
@@ -0,0 +1,34 @@
+//
+//  StockSearchViewController.m
+//  StockSearch
+//
+//  Created by Connor O'Sullivan on 3/24/12.
+//  Copyright (c) 2012 trazmick. All rights reserved.
+//
+
+#import "StockSearchViewController.h"
+
+@interface StockSearchViewController ()
+
+@end
+
+@implementation StockSearchViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
