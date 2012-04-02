d26b0027b66ff8be8c4e217f7670bf323f1edaa3
Connor O'Sullivan <trazmick@gmail.com>
2012-04-01 23:45:34 -0700

    rename AppDelegate files

---
+++ Rebalancer/RebalancerAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  StockSearchAppDelegate.h
+//  StockSearch
+//
+//  Created by Connor O'Sullivan on 3/24/12.
+//  Copyright (c) 2012 trazmick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RebalancerAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
--- Rebalancer/main.m
+++ Rebalancer/main.m
@@ -8,11 +8,11 @@
 
 #import <UIKit/UIKit.h>
 
-#import "StockSearchAppDelegate.h"
+#import "RebalancerAppDelegate.h"
 
 int main(int argc, char *argv[])
 {
     @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([StockSearchAppDelegate class]));
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RebalancerAppDelegate class]));
     }
 }
